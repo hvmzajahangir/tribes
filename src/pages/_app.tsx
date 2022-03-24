4ea40ff22851ff3ec839ad19e8dd4ffa1245d426
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-03-24 23:36:11 +0000

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { initialize, Provider } from '@decentology/hyperverse';
+import { Network } from '@decentology/hyperverse';
+import { Ethereum } from '@decentology/hyperverse-ethereum';
+import * as Tribes from '@decentology/hyperverse-evm-tribes';
+import * as RandomPick from '@decentology/hyperverse-ethereum-randompick';
+import InnerComponent from '../components/InnerComponent';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Change your Tenant ID here.
+const TENANT_ID = '0x62a7aa79a52591Ccc62B71729329A80a666fA50f';
+
+function MyApp({ Component, pageProps }: AppProps) {
+	const hyperverse = initialize({
+		blockchain: Ethereum,
+		network: Network.Testnet,
+		storage: {
+			options: { clientUrl: 'https://fileportal.org' }, // Updated to fileportal due to SSL error issues with SiaSky.net
+		},
+		modules: [
+			{
+				bundle: Tribes,
+				tenantId: TENANT_ID,
+			},
+			{
+				bundle: RandomPick,
+				tenantId: TENANT_ID,
+			},
+		],
+	});
+	return (
+		<Provider initialState={hyperverse}>
+			<InnerComponent>
+				<ToastContainer />
+				<Component {...pageProps} />
+			</InnerComponent>
+		</Provider>
+	);
+}
+
+export default MyApp;
